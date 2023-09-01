30fa61124192fb1698f6c1fd8e3d5d1a8df12731
Bondu Mohan Srinivasa Sarma <54416419+BonduMohanSrinivasaSarma@users.noreply.github.com>
2023-09-01 20:17:45 +0530

    Revert "Update feedback.php"
    This reverts commit 1b91d958e7f54b2631490384e97bf61870f2e0db.

--- feedback.php
+++ feedback.php
@@ -17,8 +17,6 @@
     <td>
     <?php
         $connection = mysqli_connect('sql302.epizy.com', 'epiz_28810806', 'IiDZQFiD0nm','epiz_28810806_Labproject');
-
-        
         $query = $connection->query("SELECT DISTINCT Facultyname FROM facultyinfo ORDER BY Facultyname");
         $query1= $connection->query("SELECT DISTINCT course FROM facultyinfo ORDER BY course ");
         $query2= $connection->query("SELECT DISTINCT slot FROM facultyinfo ORDER BY slot");
